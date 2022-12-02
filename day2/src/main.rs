3e230ad21f1cfa36d33ed98e3749da71c595bd50
Andrew Rogers <andrurogerz@gmail.com>
2022-12-02 10:21:03 -0800

    day 2, part 1 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,15 +8,15 @@ fn calculate_score(lines : impl Iterator<Item = io::Result<String>>) -> u64 {
     assert_eq!(chars.len(), 3);
     assert_eq!(chars[1], ' ');
     let round_score = match (chars[0], chars[2]) {
-      ('A', 'X') => { 1 + 3 }, // them:rock, me:rock => 4
-      ('A', 'Y') => { 2 + 6 }, // them:rock, me:paper => 8
-      ('A', 'Z') => { 3 + 0 }, // them:rock, me:scissors => 3
-      ('B', 'X') => { 1 + 0 }, // them:paper, me:rock => 1
-      ('B', 'Y') => { 2 + 3 }, // them:paper, me:paper => 5
-      ('B', 'Z') => { 3 + 6 }, // them:paper, me:scissors => 9
-      ('C', 'X') => { 1 + 6 }, // them:scissors, me:rock => 7
-      ('C', 'Y') => { 2 + 0 }, // them:scissors, me:paper => 2
-      ('C', 'Z') => { 3 + 3 }, // them:scissors, me:scissors => 6
+      ('A', 'X') => { 1 + 3 }, // them:rock, me:rock
+      ('A', 'Y') => { 2 + 6 }, // them:rock, me:paper
+      ('A', 'Z') => { 3 + 0 }, // them:rock, me:scissors
+      ('B', 'X') => { 1 + 0 }, // them:paper, me:rock
+      ('B', 'Y') => { 2 + 3 }, // them:paper, me:paper
+      ('B', 'Z') => { 3 + 6 }, // them:paper, me:scissors
+      ('C', 'X') => { 1 + 6 }, // them:scissors, me:rock
+      ('C', 'Y') => { 2 + 0 }, // them:scissors, me:paper
+      ('C', 'Z') => { 3 + 3 }, // them:scissors, me:scissors
       (a, b) => {
         // Assume only well-structure input.
         panic!("invalid input: ({}, {})", a, b);
