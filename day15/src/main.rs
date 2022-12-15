8a91f6a23512b23bad0f6b6ff8b2a6ed8a9908e5
Andrew Rogers <andrurogerz@gmail.com>
2022-12-15 14:24:33 -0800

    day 15, part 1 solution

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,5 +1,7 @@
+use std::cmp;
 use std::collections::HashMap;
 use std::error::Error;
+use std::fmt;
 use std::io;
 
 #[derive(Hash, Clone, Copy, Debug, Eq, PartialEq)]
@@ -8,9 +10,101 @@ struct Position {
   y: i32,
 }
 
+impl Position {
+  fn distance_from(&self, other: &Position) -> i32 {
+    let dx: i32 = self.x - other.x;
+    let dy: i32 = self.y - other.y;
+    dx.abs() + dy.abs()
+  }
+}
+
+#[derive(Clone)]
+struct Grid {
+  min: Position,
+  max: Position,
+  positions: HashMap<Position, Position>,
+}
+
+impl Grid {
+  fn insert(&mut self, sensor_pos: Position, beacon_pos: Position) {
+    let dist = sensor_pos.distance_from(&beacon_pos);
+
+    // Update grid bounds.
+    self.min.x = cmp::min(self.min.x, sensor_pos.x - dist);
+    self.min.y = cmp::min(self.min.y, sensor_pos.y - dist);
+    self.max.x = cmp::max(self.max.x, sensor_pos.x + dist);
+    self.max.y = cmp::max(self.max.y, sensor_pos.y + dist);
+
+    self.positions.insert(sensor_pos, beacon_pos);
+  }
+
+  fn row_count_definitely_not_beacons(&self, row: i32) -> usize {
+    let mut count = 0usize;
+    for x in self.min.x..=self.max.x {
+      let current_pos = Position { x, y: row };
+      for (&sensor_pos, &beacon_pos) in &self.positions {
+        let max_dist = sensor_pos.distance_from(&beacon_pos);
+        let actual_dist = sensor_pos.distance_from(&current_pos);
+        if current_pos == beacon_pos {
+          // Already a beacon at this position.
+          break;
+
+        } else if current_pos == sensor_pos {
+          // A sensor in this position, so can't be a beacon.
+          count += 1;
+          break;
+
+        } else if actual_dist <= max_dist {
+          // Within manhattan distance of the sensor, so can't be a beacon.
+          count += 1;
+          break;
+        }
+        // Else, this position could possibly be a beacon.
+      }
+    }
+
+    count
+  }
+}
+
+impl fmt::Debug for Grid {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    for y in self.min.y..=self.max.y {
+      print!("{0:4}: ", y);
+      for x in self.min.x..=self.max.x {
+        let current_pos = Position { x, y };
+        let mut ch = '?';
+        for (&sensor_pos, &beacon_pos) in &self.positions {
+          let max_dist = sensor_pos.distance_from(&beacon_pos);
+          let actual_dist = sensor_pos.distance_from(&current_pos);
+          if current_pos == beacon_pos {
+            ch = 'B';
+            break;
+          } else if current_pos == sensor_pos {
+            ch = 'S';
+            break;
+          } else if actual_dist <= max_dist {
+            ch = '#';
+            break;
+          }
+        }
+
+        write!(f, "{}", ch)?;
+      }
+      writeln!(f, "")?;
+    }
+
+    fmt::Result::Ok(())
+  }
+}
+
 fn parse_input(lines: &mut impl Iterator<Item = io::Result<String>>)
-    -> Result<HashMap<Position, Position>, Box<dyn Error>> {
-  let mut positions = HashMap::new();
+    -> Result<Grid, Box<dyn Error>> {
+  let mut grid = Grid {
+     min: Position { x: i32::MAX, y: i32::MAX },
+     max: Position { x: i32::MIN, y: i32::MIN },
+     positions: HashMap::new(),
+  };
 
   for line in lines {
     let line = line?;
@@ -42,22 +136,26 @@ fn parse_input(lines: &mut impl Iterator<Item = io::Result<String>>)
     assert_eq!(text, &line[tok_end + 1..tok_end + 1 + text.len()]);
     let beacon_y= line[tok_start..].parse::<i32>()?;
 
-    assert!(!positions.contains_key(&Position { x: sensor_x, y: sensor_y }));
-    assert!(!positions.contains_key(&Position { x: beacon_x, y: beacon_y }));
+    assert!(!grid.positions.contains_key(&Position { x: sensor_x, y: sensor_y }));
+    assert!(!grid.positions.contains_key(&Position { x: beacon_x, y: beacon_y }));
 
-    positions.insert(
+    grid.insert(
       Position { x: sensor_x, y: sensor_y },
       Position { x: beacon_x, y: beacon_y });
   }
 
-  Ok(positions)
+  Ok(grid)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let positions = parse_input(&mut io::stdin().lines())?;
+  let grid = parse_input(&mut io::stdin().lines())?;
 
   #[cfg(debug_assertions)]
-  println!("{:?}", positions);
+  println!("{:?}", grid);
+
+  const TEST_ROW : i32 = 2000000;
+  let result = grid.row_count_definitely_not_beacons(TEST_ROW);
+  println!("part 1: {}", result);
 
   Ok(())
 }
