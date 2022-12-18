570eafffeeed65b2c916069fae952c9bb2e7b8bb
Andrew Rogers <andrurogerz@gmail.com>
2022-12-18 08:52:24 -0800

    day 16, part 1 solution

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
@@ -95,6 +96,10 @@ fn parse_input(lines: &mut impl Iterator<Item = io::Result<String>>)
   Ok(valve_graph)
 }
 
+// Reduce the graph by "inlining" all nodes with flow rate of zero and removing
+// them from the graph. Edge costs are updated to reflect the cost of traversing
+// "through" the zero flow rate nodes. The start node is left intact even if it
+// has a flow rate of zero.
 fn reduce_graph(start_valve_id : ValveId, valve_graph: &mut ValveGraph) {
   let mut valves_to_remove : HashSet<ValveId> = HashSet::new();
 
@@ -118,7 +123,7 @@ fn reduce_graph(start_valve_id : ValveId, valve_graph: &mut ValveGraph) {
       for (&next_valve_id, &next_distance) in &valve.adjacent_valves {
         assert!(next_valve_id != valve_id);
         let next_valve = valve_graph.get(&next_valve_id).unwrap();
-        if next_valve.flow_rate == 0 {
+        if next_valve.flow_rate == 0 && next_valve_id != start_valve_id {
           // Inline the path from valve to next_valve.
           for (&next_next_valve_id, next_next_distance) in &next_valve.adjacent_valves {
             if next_next_valve_id != valve_id &&
@@ -172,6 +177,102 @@ fn reduce_graph(start_valve_id : ValveId, valve_graph: &mut ValveGraph) {
   }
 }
 
+// Find the shortest path between two nodes in the graph.
+fn find_shortest_path(start_id: ValveId, end_id: ValveId, graph: &ValveGraph, visited: &HashSet<ValveId>) -> usize {
+  assert!(graph.contains_key(&start_id));
+  assert!(graph.contains_key(&end_id));
+  assert!(!visited.contains(&start_id));
+  assert!(!visited.contains(&end_id));
+
+  if start_id == end_id {
+    return 0;
+  }
+
+  let mut visited = visited.clone();
+  visited.insert(start_id);
+
+  let valve = graph.get(&start_id).unwrap();
+  let mut dist = usize::MAX;
+  for (&next_id, next_dist) in &valve.adjacent_valves {
+    if visited.contains(&next_id) {
+      continue;
+    }
+
+    let result = find_shortest_path(next_id, end_id, graph, &visited);
+    if result == usize::MAX {
+      continue;
+    }
+
+    dist = cmp::min(dist, next_dist + result);
+  }
+
+  dist
+}
+
+// Create a copy of the input graph where every node is connected to every other
+// node with an edge reflecting the cost to traverse to it.
+fn connected_graph_create(valve_graph: &ValveGraph) -> ValveGraph {
+  let mut connected_graph = HashMap::new();
+
+  // Build a fully connected graph.
+  let valve_ids: Vec<ValveId> = valve_graph.keys().cloned().collect();
+  for i in 0..valve_ids.len() {
+    let mut valve_edges = HashMap::new();
+    for j in 0..valve_ids.len() {
+      if i == j {
+        continue;
+      }
+
+      valve_edges.insert(
+          valve_ids[j],
+          find_shortest_path(valve_ids[i], valve_ids[j], valve_graph, &HashSet::new()));
+    }
+
+    let valve = valve_graph.get(&valve_ids[i]).unwrap();
+    connected_graph.insert(
+        valve_ids[i],
+        Valve { flow_rate: valve.flow_rate, adjacent_valves: valve_edges });
+  }
+
+  connected_graph
+}
+
+// Calculate the max pressure that can be released by releasing remaining valves
+// in the optimal order given the remaining time.
+fn find_max_pressure_released(valve_id: ValveId, connected_graph: &ValveGraph, time_remaining: usize) -> usize {
+  assert!(time_remaining > 0);
+
+  let mut pressure_released = 0usize;
+  let current_valve = connected_graph.get(&valve_id).unwrap();
+
+  // Create an updated version of the connected graph with this valve removed
+  // so it doesn't get visited again.
+  let mut updated_graph = connected_graph.clone();
+  updated_graph.remove(&valve_id);
+
+  // Try traversing to every remaining node in the graph and determine which one
+  // will give us the max pressure released. The updated graph may now be empty,
+  // in which case we've hit the base case and will make no more recursive calls.
+  for &next_valve_id in updated_graph.keys() {
+    let time_cost = current_valve.adjacent_valves.get(&next_valve_id).unwrap();
+
+    if time_cost + 1 >= time_remaining {
+      // Not enough time to traverse to and release this valve.
+      continue;
+    }
+
+    // Calculate time remaining if we traverse to this valve and release it.
+    let next_time_remaining = time_remaining - time_cost - 1;
+
+    // Recursively calculate the pressure released from the next valve and the
+    // remaining graph.
+    pressure_released = cmp::max(pressure_released,
+        find_max_pressure_released(next_valve_id, &updated_graph, next_time_remaining));
+  }
+
+  pressure_released + (current_valve.flow_rate * time_remaining)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
   let mut valve_graph = parse_input(&mut io::stdin().lines())?;
 
@@ -183,6 +284,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
   println!("reduced graph:");
   print_valve_graph(&valve_graph);
+
+  let connected_graph = connected_graph_create(&valve_graph);
+
+  println!("connected graph:");
+  print_valve_graph(&connected_graph);
+
+  let result = find_max_pressure_released(start_valve_id, &connected_graph, 30);
+  println!("part 1: {}", result);
 
   Ok(())
 }
